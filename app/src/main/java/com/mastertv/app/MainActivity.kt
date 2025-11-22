c7f865ef90096f2213eff959c07abbbadc9f7b3c
Athirsonbc <athirsoncaraciolo22@gmail.com>
2025-11-22 16:37:37 -0300

    Initial commit – Master TV Android TV project

---
+++ app/src/main/java/com/mastertv/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mastertv.app
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Vamos implementar a UI após conexão com API e M3U
+    }
+}
